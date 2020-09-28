cbb40b23ec1bbbc9495354b8bbc0c63a353b5600
12mohaned <mohaned_boss@outlook.com>
2020-09-28 12:41:29 +0200

    Footer Component

---
+++ frontend/src/shared/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+class Footer extends React.Component{
+  render(){
+  return (
+    <div className = 'Footer'>
+    <footer class="page-footer orange lighten-1">
+            <div class="container">
+              <div class="row">
+                <div class="col l6 s12">
+                  <p class="grey-text text-lighten-4">Made by Mohaned Mashaly with love</p>
+                  <a class="grey-text text-lighten-3" href="https://github.com/12mohaned">Github</a>
+                </div>
+              </div>
+            </div>
+            <div class="footer-copyright">
+              <div class="container">
+              callForHelp
+              </div>
+            </div>
+          </footer>
+    </div>
+  )
+}
+}
+export default Footer;
