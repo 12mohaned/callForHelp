3349bb17a63d2204f3f5437e4b7c09b66a258612
12mohaned <mohaned_boss@outlook.com>
2020-10-12 17:04:27 +0200

    add countries and their images in database

--- Visuals/admin.py
+++ Visuals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Country
+
+class CountryAdmin(admin.ModelAdmin):
+    fields = ["name", "image"]
+admin.site.register(Country, CountryAdmin)
--- Visuals/models.py
+++ Visuals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(primary_key = True, max_length = 100)
+    image = models.ImageField(upload_to = 'visuals/assets')
+
+    def __str__(self):
+        return self.name
