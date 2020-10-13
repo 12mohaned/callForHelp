efca5957190fa05ed4d8790590fd84afa8d3ba62
12mohaned <mohaned_boss@outlook.com>
2020-10-13 19:06:18 +0200

    Add Model Manager

--- Visuals/models.py
+++ Visuals/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class CountryManager(models.Manager):
+    def edit_country_name(self, pre_name, curr_name):
+        country = Country.objects.get(name = pre_name)
+        country.name = curr_name
+        country.save()
+        previous_country = Country.objects.get(name = pre_name)
+        previous_country.delete()
+        return country
+
+    def create_country(self, name, image):
+        country = self.create(name = name, image = image)
+        return country
+
 class Country(models.Model):
     name = models.CharField(primary_key = True, max_length = 100)
     image = models.ImageField(upload_to = 'visuals/assets')
-
+    objects = CountryManager()
     def __str__(self):
         return self.name
