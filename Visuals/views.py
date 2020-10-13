b2ffdfb61be8d5de125c16a8ced25131d4d4d5c8
12mohaned <mohaned_boss@outlook.com>
2020-10-13 19:05:58 +0200

    add end-points of the countries API

--- Visuals/views.py
+++ Visuals/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 import pandas as pd
 import os
 import numpy as np
-from .models import Country
-dataframe =pd.read_csv(os.path.join(r"C:\Users\mohan\Machine-learning-models\datasets",'share-deaths-suicide.csv'))
-countries = np.unique(dataframe['Entity'])
+from .models import Country, CountryManager
+from django.core.files import File
+from Visuals.serializers import CountrySerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+import requests
 
 def Visuals(request):
+    Country.objects.edit_country_name('United States Virgin Islands', 'Mohaned')
     return render(request, 'Home.html')
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all().order_by('name')
+    serializer_class = CountrySerializer
