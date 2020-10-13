9c64a805057ce75d1d8a09e35098284447cc1739
12mohaned <mohaned_boss@outlook.com>
2020-10-13 19:05:14 +0200

    API url

--- Visuals/urls.py
+++ Visuals/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework import routers
+from django.urls import include
+from Visuals.views import CountryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'countries', CountryViewSet, basename = 'countries')
 
 urlpatterns = [
-path('Visuals', views.Visuals, name = 'visuals')
+path('Visuals', views.Visuals, name = 'visuals'),
+path('api',include(router.urls)),
+path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
